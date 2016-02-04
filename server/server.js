9b17887474f1e37dfb89b94e687722727755bb22
Jon Tippens <jwtippens@gmail.com>
2016-02-03 19:21:28 -0800

    Added MVP Globe

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
-var express = require('express');
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.config.js';
+
+// var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 

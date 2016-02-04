3893b48a7ddfc905cc5ba479927037cd266b31f2
Juan <jguardado85@gmail.com>
2016-02-03 17:15:25 -0800

    sequelize installed and saving to db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var db = require('./sequelizeDB.js');
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../'));
+
 // console.log(__dirname + '/../index.html');
 app.listen(port);
 console.log('karun says server works');
